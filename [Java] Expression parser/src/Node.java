66a4e234aaef16df9211a2e3fa8c61ec01acb375
ilievlad73 <ilievlad73@gmail.com>
2017-02-17 13:48:27 +0200

    Update Node.java

--- [Java] Expression parser/src/Node.java
+++ [Java] Expression parser/src/Node.java
@@ -5,11 +5,6 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Stack;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 /**
  *
  * @author Ilie
